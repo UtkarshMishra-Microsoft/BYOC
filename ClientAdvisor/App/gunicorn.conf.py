1b5dc16ed49f377456f2ea17254182e107e15c9e
UtkarshMishra-Microsoft <v-utkamishra@microsoft.com>
2024-12-17 14:29:27 +0530

    test1

--- ClientAdvisor/App/gunicorn.conf.py
+++ ClientAdvisor/App/gunicorn.conf.py
@@ -1,13 +1,13 @@
 import multiprocessing
 
-max_requests = 1000
-max_requests_jitter = 50
-log_file = "-"
-bind = "0.0.0.0"
+MAX_REQUESTS = 1000
+MAX_REQUESTS_JITTER = 50
+LOG_FILE = "-"
+BIND = "0.0.0.0"
 
-timeout = 230
+TIMEOUT = 230
 # https://learn.microsoft.com/en-us/troubleshoot/azure/app-service/web-apps-performance-faqs#why-does-my-request-time-out-after-230-seconds
 
 num_cpus = multiprocessing.cpu_count()
 workers = (num_cpus * 2) + 1
-worker_class = "uvicorn.workers.UvicornWorker"
+WORKER_CLASS = "uvicorn.workers.UvicornWorker"
